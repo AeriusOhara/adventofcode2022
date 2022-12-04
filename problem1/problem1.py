c0570a2de5be1322d07570f956bd4aa9e9e455b5
Salih Ozkan <it.salih.ozkan@gmail.com>
2022-12-04 11:15:34 +0100

    Problem 1, part 2 solved, 195625 calories

--- problem1/problem1.py
+++ problem1/problem1.py
@@ -12,3 +12,13 @@ with open("input.txt") as file:
 
 elvesList.sort(reverse=True)
 print(f"The elf holding the largest amount of calories holds: {elvesList[0]} calories.")
+
+# Part 2
+top_three_elves_calories = 0
+i = 0
+while i < 3:
+    top_three_elves_calories += elvesList[i]
+    
+    i += 1
+
+print(f"The top 3 elves' calories combined are: {top_three_elves_calories}")
