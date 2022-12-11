cb26d34f99ef692d3dc0cbc44a8c628b10fdbceb
= <=>
2022-12-11 21:51:23 +0100

    Finished part 2 as well.

--- problem2/problem2_part2.py
+++ problem2/problem2_part2.py
@@ -1,52 +1,60 @@
-# X = lose
-# Y = draw
-# Z = win
-
 def getIntValue(_input):
+    # Turn A, B, C into 1, 2 and 3
     return (ord(_input)  - 64)
 
-def determineWeakness(_input, shouldWin):
-    if(_input == "A"):
+def determineOutcome(_input, shouldWin=True):
+    if(_input == "A"): # Rock
         if(shouldWin): return "B"
         else: return "C"
-    elif(_input == "B"):
+    elif(_input == "B"): # Paper
         if(shouldWin): return "C"
         else: return "A"
-    elif(_input == "C"):
+    elif(_input == "C"): # Scissor
         if(shouldWin): return "A"
         else: return "B"
     else:
-        print("determineWeakness() :: Wrong input received: " + _input)
+        print("determineOutcome() :: Wrong input received: " + _input)
 
 
-def determineOurMove(_input1, _outcome):
+def determineOurMove(_input, _outcome):
     if _outcome == "X":
-        #
-    elif _outcome == "Z"
-
-    return score
+        # Get lose result
+        return determineOutcome(_input, False)
+    elif _outcome == "Y":
+        # Get draw result
+        return _input
+    elif _outcome == "Z":
+        # Get win result
+        return determineOutcome(_input)
 
 score = 0
+ourAction = ""
 with open("input.txt") as file:
     for line in file:
         curLine = line.strip().split(" ")
         opponentMove = curLine[0]
         roundConclusion = curLine[1]
 
-        # Convert the moves to the same character for easy comparison
-        #if(playerMove == "X"): playerMove = "A"
-        #elif(playerMove == "Y"): playerMove = "B"
-        #elif(playerMove == "Z"): playerMove = "C"
-
+        # Give us scores based on the result
+        # we're supposed to have
         if(roundConclusion == "Y"):
             # Draw
             score += 3
         elif(roundConclusion == "Z"):
-            # We have to win
+            # Win
             score += 6
 
-        score += determineOurMove(opponentMove, roundConclusion)
-#print("Score: " + str(score))
+        # Determine the move we made and give
+        # the appropriate score
+        ourAction = determineOurMove(opponentMove, roundConclusion)
+        if(ourAction == "A"):
+            # We used rock
+            score += 1
+        elif(ourAction == "B"):
+            # We used paper
+            score += 2
+        else:
+            # We used scissor
+            score += 3
 
-
-# Print simple tests
+print("Score: " + str(score))
